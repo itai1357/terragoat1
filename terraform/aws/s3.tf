b20fcef22b0fd013bc393adcc8f51a40cf5d8655
Saar Ettinger <Saarett@users.noreply.github.com>
2021-07-08 18:16:00 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -21,6 +21,16 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
+resource "aws_s3_bucket" "saar" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-saar"
+  acl           = "public-read"
+  force_destroy = true
+}
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
